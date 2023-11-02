77e76f98c6972a1dba27c6cfe6cc44a8132dc978
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-11-02 19:48:04 -0300

    file input styling

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -10,9 +10,10 @@ export default function LoginForm() {
   const [password, setPassword] = useState('')
   const store = useStore()
 
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    if(formRef.current) {
+    if (formRef.current) {
       const formData = new FormData(formRef.current)
       const user = extractInfo(formData)
       store.setUser(user)
@@ -22,10 +23,12 @@ export default function LoginForm() {
 
   return (
     <form onSubmit={handleSubmit} className='flex flex-col gap-7' ref={formRef}>
-      <label htmlFor="">
-        <img src={Image} alt="profile picture" />
-        <input type="file" id='profilePicture' name='profilePicture' required/>
-        Sube tu foto de perfil
+      <label htmlFor="profilePicture" className='flex lg:flex-row sm:flex-col lg:text-xl sm:text-lg lg:items-center hover:cursor-pointer lg:justify-between sm:justify-center'>
+        <div className='flex items-center gap-2'>
+          <img src={Image} alt="profile picture" className='border-2 border-border bg-background rounded-full w-[3.1rem] p-2' />
+          Elegí una foto de perfil:
+        </div>
+        <input type="file" id='profilePicture' name='profilePicture' className='file:hidden lg:w-1/3 lg:p-0 sm:w-full sm:px-10' autoComplete='photo' required />
       </label>
       <input type="text" name="fullName" placeholder='Nombre y Apellido' className='bg-background border-2 p-3 rounded-lg border-border focus:outline-none' autoComplete='fullName' required />
       <input type="tel" name="cellPhone" placeholder='+541123685458' className='bg-background border-2 p-3 rounded-lg border-border focus:outline-none' autoComplete='telephone' required />
