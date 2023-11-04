d06eee7f22b9c1fca82bfa636c7e222e2135c6c9
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-11-04 19:44:11 -0300

    fix: login validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -12,7 +12,7 @@ type FormValues = {
 export default function LoginForm() {
   const [passwordShown, setPasswordShown] = useState(false);
   const passRef = useRef<HTMLInputElement | null>(null)
-  const { register, handleSubmit } = useForm<FormValues>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
   const { ref } = register('password')
   useImperativeHandle(ref, () => passRef.current)
   const state = sessionStorage.getItem('userState')
@@ -25,7 +25,7 @@ export default function LoginForm() {
     const emailCheck = user.email === data.email
     const passwordCheck = user.password === data.password
 
-    if(emailCheck && passwordCheck) {
+    if (emailCheck && passwordCheck) {
       loginSuccess(user)
     } else {
       loginFailure(user.email)
@@ -39,7 +39,9 @@ export default function LoginForm() {
           type="email"
           placeholder='hola@tuemail.com'
           autoComplete='username'
-          {...register("email", { required: true })}
+          {...register("email", { 
+            required: true
+          })}
           className='bg-background border-2 p-3 rounded-lg border-border focus:outline-none'
         />
       </label>
@@ -50,13 +52,17 @@ export default function LoginForm() {
             id='password'
             placeholder='Ingresá tu contraseña'
             className='w-[100%] bg-background p-3 focus:outline-none' autoComplete='current-password'
-            {...register("password", { required: true })}
+            {...register("password", { 
+              required: true
+            })}
             ref={passRef}
           />
+          {errors?.email ? <p className='text-center p-1 text-red-400'>{errors.email?.message as string}</p> : null}
           <button className='p-2' type='button' onClick={() => setPasswordShown(!passwordShown)}>
             {passInput === 'password' ? <AiOutlineEyeInvisible className="text-2xl opacity-50" /> : <AiOutlineEye className="text-2xl opacity-50" />}
           </button>
         </div>
+        {errors?.password ? <p className='text-center p-1 text-red-400'>{errors.password?.message as string}</p> : null}
       </label>
       <button type="submit" className='bg-primary text-white border-2 p-3 rounded-lg font-bold'>Iniciar Sesión</button>
     </form>
